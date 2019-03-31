3c770aca3414df69b79dd88ce59e48b7df23ed19
Ítalo Fernandes <italogsfernandes@gmail.com>
2019-04-01 01:56:12 +0200

    password reset

--- im2webapp/django_project/urls.py
+++ im2webapp/django_project/urls.py
@@ -3,7 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 
 from users.views import register, profile
 
@@ -17,6 +24,26 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(
         template_name='users/logout.html'), name='logout'
     ),
+    path('password-reset/',
+         PasswordResetView.as_view(
+             template_name='users/password_reset.html'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
     path('', include('im2webapp.urls')),
 ]
 
