4f3c2920a02b52778ed209a9a6ba3cf1860c9175
Ítalo Fernandes <italogsfernandes@gmail.com>
2019-04-01 01:24:40 +0200

    login logout authentication

--- im2webapp/django_project/urls.py
+++ im2webapp/django_project/urls.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
-from users.views import register
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib.auth.views import LoginView, LogoutView
+
+from users.views import register, profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
+    path('login/', LoginView.as_view(
+        template_name='users/login.html'), name='login'
+    ),
+    path('logout/', LogoutView.as_view(
+        template_name='users/logout.html'), name='logout'
+    ),
     path('', include('im2webapp.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
