2e6a5499af1b5c15f1e2c621f19baa75b53a581a
Ítalo Fernandes <italogsfernandes@gmail.com>
2019-04-01 02:05:12 +0200

    Fixed: user profile save and signals

--- im2webapp/users/signals.py
+++ im2webapp/users/signals.py
@@ -5,11 +5,8 @@ from .models import Profile
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    try:
+        instance.profile.save()
+    except Profile.DoesNotExist:
+        Profile.objects.create(user=instance)
