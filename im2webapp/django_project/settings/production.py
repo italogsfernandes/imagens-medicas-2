507d0cc3fa035c28094c2c7e7509acc29e5a9e01
Ítalo Fernandes <italogsfernandes@gmail.com>
2019-04-11 14:24:16 +0200

    changes in prod db

--- im2webapp/django_project/settings/production.py
+++ im2webapp/django_project/settings/production.py
@@ -7,7 +7,7 @@ DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_IM2WEBAPP_SECRET_KEY')
 
-DATABASES['default']['USER'] = 'http'  # NOQA
+# DATABASES['default']['USER'] = 'http'  # NOQA
 
 
 ALLOWED_HOSTS = [
