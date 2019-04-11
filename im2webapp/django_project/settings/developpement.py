ba26d9379b36e1875308d6b6bdddd774b273e302
Ítalo Fernandes <italogsfernandes@gmail.com>
2019-04-11 14:17:15 +0200

    secret key as os env var

--- im2webapp/django_project/settings/developpement.py
+++ im2webapp/django_project/settings/developpement.py
@@ -7,7 +7,7 @@ for template_engine in TEMPLATES:  # NOQA
     template_engine['OPTIONS']['debug'] = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!-mi(aqg))3348d$%g0e%plgxtrgl5#m!3a-e3h9vmwuz%(r6i'
+SECRET_KEY = os.environ.get('DJANGO_IM2WEBAPP_SECRET_KEY')
 
 try:
     from .local import *  # NOQA
--- im2webapp/django_project/settings/production.py
+++ im2webapp/django_project/settings/production.py
@@ -5,7 +5,7 @@ from .base import *  # NOQA
 DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!-mi(aqg))3348d$%g0e%plgxtrgl5#m!3a-e3h9vmwuz%(r6i'
+SECRET_KEY = os.environ.get('DJANGO_IM2WEBAPP_SECRET_KEY')
 
 DATABASES['default']['USER'] = 'http'  # NOQA
 
