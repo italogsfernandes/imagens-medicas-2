ce8de4688b219710ec7828e9c17452a989e6defe
Ítalo Fernandes <italogsfernandes@gmail.com>
2019-04-11 14:01:28 +0200

    tests db

--- im2webapp/django_project/settings/tests.py
+++ im2webapp/django_project/settings/tests.py
@@ -1,3 +1,17 @@
 from os.path import join
 
 from .developpement import *  # NOQA
+
+SECRET_KEY = "57c%giio-i327pa+c*w5)^$hpemx35s@c3*2%ncgu9fp#pxc9r"
+DEBUG = False
+TEMPLATE_DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'travis_ci_db',
+        'USER': 'travis',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+    }
+}
