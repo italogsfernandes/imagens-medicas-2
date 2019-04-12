a98d4272ae2e26ec43a96106ac62ffaca95909db
Ítalo Fernandes <italogsfernandes@gmail.com>
2019-04-12 17:18:49 +0200

    security improvements

--- im2webapp/django_project/settings/base.py
+++ im2webapp/django_project/settings/base.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER_API = os.environ.get('DJANGO_IM2WEBAPP_EMAIL_HOST_USER_API')
+EMAIL_HOST_PASSWORD_KEY = os.environ.get('DJANGO_IM2WEBAPP_EMAIL_HOST_PASSWORD_KEY')
--- im2webapp/django_project/settings/developpement.py
+++ im2webapp/django_project/settings/developpement.py
@@ -4,11 +4,17 @@ from .base import *  # NOQA
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'italogsfernandes.com',
+]
+
 for template_engine in TEMPLATES:  # NOQA
     template_engine['OPTIONS']['debug'] = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_IM2WEBAPP_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_IM2WEBAPP_DEVELOPPEMENT_SECRET_KEY')
 
 try:
     from .local import *  # NOQA
