dba257e73a10fe1ffc6c051e6e8569178dd1888f
Ítalo Fernandes <italogsfernandes@gmail.com>
2019-04-12 17:42:56 +0200

    locales not fixed

--- im2webapp/django_project/settings/base.py
+++ im2webapp/django_project/settings/base.py
@@ -50,7 +50,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.core.context_processors.i18n',
             ],
         },
     },
