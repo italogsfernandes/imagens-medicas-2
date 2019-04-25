4741ced5c4698930d49ce45becfbe257953c9bd5
Italo G S Fernandes <italogsfernandes@gmail.com>
2019-04-25 10:55:48 +0200

    fix dev secret key

--- im2webapp/django_project/settings/developpement.py
+++ im2webapp/django_project/settings/developpement.py
@@ -14,7 +14,10 @@ for template_engine in TEMPLATES:  # NOQA
     template_engine['OPTIONS']['debug'] = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_IM2WEBAPP_DEVELOPPEMENT_SECRET_KEY')
+SECRET_KEY = os.environ.get(
+    'DJANGO_IM2WEBAPP_DEVELOPPEMENT_SECRET_KEY',
+    'lrqa@71ktxw8rgkm&w9w41w4s5bxvn83+qxdzdggt6z=8qrgkz', # Default value
+)
 
 try:
     from .local import *  # NOQA
