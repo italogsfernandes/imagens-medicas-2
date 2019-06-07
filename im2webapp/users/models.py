055cb78fc424beba86e3dd14c060f6dd27bf805d
Italo G S Fernandes <italogsfernandes@gmail.com>
2019-06-07 10:36:46 +0200

    default pic file not gitignored

--- im2webapp/users/models.py
+++ im2webapp/users/models.py
@@ -2,14 +2,11 @@ from PIL import Image
 
 from django.db import models
 from django.contrib.auth.models import User
-from django_project import settings
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(
-        default='..{}images/profile-default.jpg'.format(settings.STATIC_URL),
-        upload_to='profile_pics'
-    )
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return '{} Profile'.format(self.user.username)
