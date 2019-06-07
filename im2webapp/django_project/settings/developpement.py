7498387972bfeeecccf9d0546e8fd1d17c0fb691
Italo G S Fernandes <italogsfernandes@gmail.com>
2019-06-07 17:12:15 +0200

    dev setting connecting to db

--- im2webapp/django_project/settings/developpement.py
+++ im2webapp/django_project/settings/developpement.py
@@ -4,6 +4,20 @@ from .base import *  # NOQA
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+if os.environ.get('DJANGO_IM2WEBAPP_PRODUCTION_POSTGRESQL_PASSWORD'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'NAME': 'im2webapp',
+            'USER': os.environ.get('DJANGO_IM2WEBAPP_PRODUCTION_POSTGRESQL_USER'),
+            'PASSWORD': os.environ.get(
+                'DJANGO_IM2WEBAPP_PRODUCTION_POSTGRESQL_PASSWORD'),
+            'HOST': os.environ.get('DJANGO_IM2WEBAPP_PRODUCTION_POSTGRESQL_HOST'),
+            'PORT': os.environ.get('DJANGO_IM2WEBAPP_PRODUCTION_POSTGRESQL_PORT'),
+        }
+    }
+
+
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
