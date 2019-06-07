b5f5bee00413ecc929f75f53f84ec00eb8370b8b
Italo G S Fernandes <italogsfernandes@gmail.com>
2019-06-07 10:19:04 +0200

    static image

--- im2webapp/users/models.py
+++ im2webapp/users/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(
-        default='im2webap/static/profile-default.jpg',
+        default='/static/images/profile-default.jpg',
         upload_to='profile_pics'
     )
 
