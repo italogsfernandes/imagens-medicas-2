89222cfe82f9e7254399ec8b3b39555cb3ed9ce8
Italo G S Fernandes <italogsfernandes@gmail.com>
2019-06-07 18:41:27 +0200

    much more migue

--- im2webapp/personal_website/views.py
+++ im2webapp/personal_website/views.py
@@ -1,9 +1,7 @@
 import os
 
 from django.views.generic import View
-from django.http import JsonResponse
-
-from django_project import settings
+from django.http import JsonResponse, HttpResponse
 
 
 class PostReceiveGitHubWebHookView(View):
@@ -37,17 +35,4 @@ class PostReceiveGitHubWebHookView(View):
     def get(self, request, *args, **kwargs):
         cmd_str = "/home/italo/server-management/hooks/post-receive.sh"
         cmd_output = os.popen(cmd_str).read()
-        # TODO: send cmd_output as a mail
-        if not cmd_output:
-            return JsonResponse({
-                "success": True,
-                "message": "Error: {} {} {}".format(
-                            event_type, delivery_id, signature)
-            })
-        else:
-            with open("/home/italo/xablaus.txt", 'w+') as cmd_file:
-                cmd_file.write(cmd_output)
-        return JsonResponse({
-            "success": True,
-            "message": "Good anakin."
-        })
+        return HttpResponse(cmd_output)
