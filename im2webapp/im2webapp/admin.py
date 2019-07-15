16ac031db7bdc304d5ec8c3f0e12aa10c44104c7
Italo G S Fernandes <italogsfernandes@gmail.com>
2019-07-15 16:24:11 +0200

    wng images admin

--- im2webapp/im2webapp/admin.py
+++ im2webapp/im2webapp/admin.py
@@ -1,3 +1,29 @@
-from django.contrib import admin # NOQA
+from django.contrib import admin
+from django.utils.translation import ugettext as _
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from im2webapp.models import MedicalImage
+
+
+class MedicalImageAdmin(admin.ModelAdmin):
+    model = MedicalImage
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = [
+        'name', 'user', 'created_date', 'modified_date', 'go_to_page'
+    ]
+    readonly_fields = ['go_to_page', ]
+
+    def go_to_page(self, instance):
+        href = reverse('image-list')
+        if instance.pk and instance.slug:
+            href = reverse('image-editor', args=[instance.slug])
+        return mark_safe(
+            '<a href="{url}">{text}</a>'.format(url=href, text=_('Go to page'))
+        )
+
+    go_to_page.allow_tags = True
+    go_to_page.short_description = _('Link')
+
+
+admin.site.register(MedicalImageAdmin)
