e74b891a9ff9069b7006478106cecf274cc31378
Italo G S Fernandes <italogsfernandes@gmail.com>
2019-07-15 16:04:10 +0200

    url page image list

--- im2webapp/im2webapp/urls.py
+++ im2webapp/im2webapp/urls.py
@@ -3,6 +3,7 @@ from .views import (
     HomeView,
     AboutView,
     ImageEditorView,
+    ImageListView
 )
 
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
     path('image-editor', ImageEditorView.as_view(), name='image-editor'),
+    path('image-list', ImageListView.as_view(), name='image-list'),
 ]
--- im2webapp/im2webapp/views.py
+++ im2webapp/im2webapp/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import View
+from django.views.generic import View, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-
+from im2webapp.models import MedicalImage
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
@@ -23,3 +24,20 @@ class ImageEditorView(View):
             'im2webapp/image-editor.html',
             {'title': 'Imagens Médicas 2'}
         )
+
+
+class ImageListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'redirect_to'
+    template_name = 'im2webapp/image-list.html'
+    model = MedicalImage
+
+    def get(self, request, *args, **kwargs):
+        if self.get_queryset().count() == 1:
+            return redirect(self.get_queryset().first())
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = super(ImageListView, self).get_queryset()
+        queryset = queryset.filter(user=self.request.user)
+        return queryset
