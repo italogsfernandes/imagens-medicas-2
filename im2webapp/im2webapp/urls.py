16e1c2b9dd190e5d664f57bacaeccc8264f5f660
Italo G S Fernandes <italogsfernandes@gmail.com>
2019-09-18 14:55:26 -0300

    belo horizonte url

--- im2webapp/im2webapp/urls.py
+++ im2webapp/im2webapp/urls.py
@@ -3,6 +3,7 @@ from .views import (
     HomeView,
     AboutView,
     ImageEditorView,
+    BeloHorizonteView,
 )
 
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
     path('image-editor', ImageEditorView.as_view(), name='image-editor'),
+    path('belo-horizonte', BeloHorizonteView.as_view(), name='belo-horizonte'),
 ]
