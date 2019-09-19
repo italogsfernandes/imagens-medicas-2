61f4dbafb9f51fbdcbe5b28adc04d428dc2fce73
Italo G S Fernandes <italogsfernandes@gmail.com>
2019-09-19 17:48:00 -0300

    url image editor possible to choose image

--- im2webapp/im2webapp/urls.py
+++ im2webapp/im2webapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
+
 from .views import (
     HomeView,
     AboutView,
     ImageEditorView,
+    ImageEditorLiteView,
     BeloHorizonteView,
 )
 
@@ -10,6 +13,11 @@ from .views import (
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
-    path('image-editor', ImageEditorView.as_view(), name='image-editor'),
+    path('image-editor/', ImageEditorLiteView.as_view(), name='image-editor'),
+    url(
+        r'^image-editor/(?P<image_slug>[-0-9A-Za-z]+)/$',
+        ImageEditorView.as_view(),
+        name='image-editor'
+    ),
     path('belo-horizonte', BeloHorizonteView.as_view(), name='belo-horizonte'),
 ]
