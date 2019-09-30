ff1a4dc69d85a83ac8aeca352d3b967cbd230f7d
Italo Fernandes <italogsfernandes@gmail.com>
2019-09-30 10:28:30 -0300

    histogram working

--- im2webapp/im2webapp/static/im2webapp/js/image-editor.js
+++ im2webapp/im2webapp/static/im2webapp/js/image-editor.js
@@ -1,61 +1,103 @@
 $(document).ready(function() {
+  var canvas = document.createElement("canvas");
+
+  function get_image_data_obj(image_id) {
+    // Get image data as array
+    var data_source_img = document.getElementById(image_id);
+    canvas.width = data_source_img.width;
+    canvas.height = data_source_img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(data_source_img, 0, 0);
+    return ctx.getImageData(
+        0, 0, data_source_img.width, data_source_img.height);
+  }
+
+  function generate_histogram(image_id, div_id, title) {
+    var image_data = get_image_data_obj(image_id);
+    // Generate Histogram Trace
+    var trace = {
+      x: image_data.data,
+      type: 'histogram',
+      autobinx: false,
+      xbins: {
+        end: 256,
+        size: 1,
+        start: 0
+      }
+    };
+    // Set data (its possible to plot more than 1)
+    var data = [trace];
+    // Configure title and labels
+    var layout = {
+      title: title,
+      xaxis: {
+        title: "Value"
+      },
+      yaxis: {
+        title: "Count"
+      }
+    };
+    // Plot
+    Plotly.newPlot(div_id, data, layout);
+  }
+
+  generate_histogram(
+    "output_original_image",
+    "div_original_histogram_result_plot",
+    "Original Histogram")
+  generate_histogram(
+    "output_image",
+    "div_output_histogram_result_plot",
+    "Output Histogram");
+  $("#btn_update_image_histogram").click(function() {
+    generate_histogram(
+      "output_image",
+      "div_output_histogram_result_plot",
+      "Output Histogram");
+  });
+
   $("#range_brightness").blur(function() {
     var amount = $(this).val();
-    amount = amount / 10;
     $("#brightness_value").text(amount);
-    var img = document.getElementById('output_image');
-    img.setAttribute('style',
-      'filter:brightness(' +
-      amount +
-      '); -webkit-filter:brightness(' +
-      amount +
-      '); -moz-filter:brightness(' +
-      amount +
-      ')'
-    );
   });
+
   $("#range_contrast").blur(function() {
     var amount = $(this).val();
-    amount = amount / 10;
     $("#contrast_value").text(amount);
-    var img = document.getElementById('output_image');
-    img.setAttribute('style',
-      'filter:contrast(' +
-      amount +
-      '); -webkit-filter:contrast(' +
-      amount +
-      '); -moz-filter:contrast(' +
-      amount +
-      ')'
-    );
   });
 
+  function show_hide_original_and_histogram(){
+    input_compare = document.getElementById("input_compare");
+    input_histogram = document.getElementById("input_histogram");
 
-  $("#input_compare").change(function() {
-    if(this.checked) {
-      $("#div_output_original_image").show();
-      if($("#input_histogram").checked) {
+    if (input_histogram.checked){
+      if (input_compare.checked){
+        $("#div_output_original_image").show();
         $("#div_output_original_histogram").show();
+        $("#div_output_histogram").show();
+      } else {
+        $("#div_output_original_image").hide();
+        $("#div_output_original_histogram").hide();
+        $("#div_output_histogram").show();
       }
     } else {
-      $("#div_output_original_image").hide();
-      if($("#input_histogram").checked) {
+      if (input_compare.checked){
+        $("#div_output_original_image").show();
         $("#div_output_original_histogram").hide();
+        $("#div_output_histogram").hide();
+      } else {
+        $("#div_output_original_image").hide();
+        $("#div_output_original_histogram").hide();
+        $("#div_output_histogram").hide();
       }
     }
+  }
+  show_hide_original_and_histogram();
+  $("#input_compare").change(function() {
+    show_hide_original_and_histogram();
   });
 
   $("#input_histogram").change(function() {
-    if(this.checked) {
-      if($("#input_compare").checked) {
-        $("#div_output_original_histogram").show();
-      }
-      $("#div_output_histogram").show();
-    } else {
-      if($("#input_compare").checked) {
-        $("#div_output_original_histogram").hide();
-      }
-      $("#div_output_histogram").hide();
-    }
+    show_hide_original_and_histogram();
   });
 });
