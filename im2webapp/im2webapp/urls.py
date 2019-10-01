a97e1109547c8a646a9fab58ef28926f5da568ad
Italo Fernandes <italogsfernandes@gmail.com>
2019-10-01 10:02:10 -0300

    fix url error

--- im2webapp/im2webapp/urls.py
+++ im2webapp/im2webapp/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
     path('lite-editor/', ImageEditorLiteView.as_view(), name='lite-editor'),
-    url(
+    path(
         r'^image-editor/(?P<image_slug>[-0-9A-Za-z]+)/$',
         ImageEditorView.as_view(),
         name='image-editor'
