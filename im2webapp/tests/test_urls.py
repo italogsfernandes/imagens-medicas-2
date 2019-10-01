df5c185260d42385a12f5b955d68aea8edae6df5
Italo Fernandes <italogsfernandes@gmail.com>
2019-09-30 21:34:54 -0300

    test urls

---
+++ im2webapp/tests/test_urls.py
@@ -0,0 +1,27 @@
+from django.urls import reverse, resolve
+
+
+class TestUrls:
+    def home_url(self):
+        path = reverse('home')
+        assert resolve(path).view_name == 'home'
+
+    def about_url(self):
+        path = reverse('about')
+        assert resolve(path).view_name == 'about'
+
+    def lite_editor_url(self):
+        path = reverse('lite-editor')
+        assert resolve(path).view_name == 'lite-editor'
+
+    def image_editor_url(self):
+        path = reverse('image-editor', kwargs={'image_slug': 'test_simulated'})
+        assert resolve(path).view_name == 'image-editor'
+
+    def images_list_url(self):
+        path = reverse('images_list')
+        assert resolve(path).view_name == 'images_list'
+
+    def belo_horizonte_url(self):
+        path = reverse('belo-horizonte')
+        assert resolve(path).view_name == 'belo-horizonte'
