4ea2740c652417fd304d455e4775eaba7ac86a91
Italo Fernandes <italogsfernandes@gmail.com>
2019-09-30 21:39:15 -0300

    setuUpClass

--- im2webapp/tests/test_views.py
+++ im2webapp/tests/test_views.py
@@ -1,4 +1,5 @@
 import pytest
+from django.test import TestCase
 
 from django.test import RequestFactory
 from django.urls import reverse
@@ -8,17 +9,23 @@ from mixer.backend.django import mixer
 
 
 @pytest.mark.django_db
-class TestViews:
+class TestViews(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestViews, cls).setUpClass()
+        cls.factory = RequestFactory()
+
     def test_lite_editor(self):
         path = reverse('lite-editor')
-        request = RequestFactory().get(path)
+        request = self.factory.get(path)
         view_instance = ImageEditorLiteView.as_view()
         response = view_instance(request)
         assert response.status_code == 200
 
     def test_images_list_authenticated(self):
         path = reverse('images_list')
-        request = RequestFactory().get(path)
+        request = self.factory.get(path)
         request.user = mixer.blend(User)
         view_instance = ImageListView.as_view()
         response = view_instance(request)
@@ -26,7 +33,7 @@ class TestViews:
 
     def test_images_list_unauthenticated(self):
         path = reverse('images_list')
-        request = RequestFactory().get(path)
+        request = self.factory.get(path)
         request.user = AnonymousUser()
         view_instance = ImageListView.as_view()
         response = view_instance(request)
